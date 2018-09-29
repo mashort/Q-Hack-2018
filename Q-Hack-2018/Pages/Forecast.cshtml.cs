37a439413ef3981c83ff6b6cdd87a867a545f656
martins <martin.short@qnrl.com>
2018-09-29 21:26:37 +0100

    Add Forecast page

---
+++ Q-Hack-2018/Pages/Forecast.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Q_Hack_2018.Pages
+{
+    public class ForecastModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
