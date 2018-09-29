bbcf0f59b9721d14b53d423a1a9b92b1bbc4510f
Jamie Prady <Jamie.Prady@qnrl.com>
2018-09-29 21:08:10 +0100

    Insert a single history category id by name

---
+++ Q-Hack-2018-DB/Procedures/InsertCalculationHistoryByName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[InsertCalculationHistoryByName]
+	@calculationDate DATETIME,
+	@categoryName NVARCHAR(50)
+AS
+	INSERT INTO CalculationHistory
+	(CalculationDate, CategoryID)
+	VALUES
+	(@calculationDate, (SELECT TOP 1 [Id] FROM Categories WHERE [Name] = @categoryName))
+RETURN 0
