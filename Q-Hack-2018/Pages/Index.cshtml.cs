7549a96540d21a7589e97194f3ca44c3c34856b1
martins <martin.short@qnrl.com>
2018-09-29 17:08:37 +0100

    Wire up home page to api data

--- Q-Hack-2018/Pages/Index.cshtml.cs
+++ Q-Hack-2018/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Q_Hack_2018.Core.Entities;
+using Q_Hack_2018.Infrastructure.Business_Logic;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -15,28 +16,7 @@ namespace Q_Hack_2018.Pages
 
         public async Task OnGetAsync()
         {
-            Categories = GetCategories();
-        }
-
-        private IList<Category> GetCategories()
-        {
-            List<Category> listOfCategories = new List<Category>();
-
-            Category c1 = new Category(1, "Entertainment");
-            Category c2 = new Category(2, "Transport");
-            Category c3 = new Category(3, "Groceries");
-            Category c4 = new Category(4, "Bills");
-            Category c5 = new Category(5, "General");
-            Category c6 = new Category(6, "Shopping");
-
-            listOfCategories.Add(c1);
-            listOfCategories.Add(c2);
-            listOfCategories.Add(c3);
-            listOfCategories.Add(c4);
-            listOfCategories.Add(c5);
-            listOfCategories.Add(c6);
-
-            return listOfCategories;
-        }
+            Categories = await new TxnProcessor().ProcessTransactions();
+        }        
     }
 }
