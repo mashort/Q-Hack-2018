6996b6b66aa71edecf8238d80ae49f7ed5972c1f
Jamie Prady <Jamie.Prady@qnrl.com>
2018-09-29 21:14:50 +0100

    delete a single history category id by name

---
+++ Q-Hack-2018-DB/Procedures/DeleteCalculationHistoryByName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[DeleteCalculationHistoryByName]
+	@calculationDate DATETIME,
+	@categoryName NVARCHAR(50)
+AS
+	DELETE FROM CalculationHistory
+	WHERE CalculationDate = @calculationDate
+	AND CategoryID = (SELECT TOP 1 [Id] FROM Categories WHERE [Name] = @categoryName)
+
+RETURN 0
