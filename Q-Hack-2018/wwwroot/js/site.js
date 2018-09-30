8ac49d620cee9b41efb2270f9dc3658f4d71fb0b
Georgina Neilson <georgina.neilson@qnrl.com>
2018-09-30 11:44:08 +0100

    Add styling

--- Q-Hack-2018/wwwroot/js/site.js
+++ Q-Hack-2018/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+
+function testFunction(event) {
+    let btn = event.target;
+    let btnsib = btn.nextElementSibling.innerHTML;
+    console.log("sib: ", btnsib.classList);
+    console.log("btn: ", btn.classList);
+    // if(btn.classList.contain("btn-section--active"){
+        
+    // })
+
+}
+// $(window).on("load", function () {
+//     // Handler when all assets (including images) are loaded
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+
+});
